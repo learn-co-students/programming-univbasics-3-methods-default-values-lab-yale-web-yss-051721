70205c658eae4fe46ec2a66deec9cb370aaddfe0
abstract-inheritance-c040 <doe.mahamud1@gmail.com>
2021-05-24 01:03:28 +0000

    Done.

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,2 +1,9 @@
 # Your code here
 # For output purposes, use "puts" instead of "print" or "p"
+def meal_choice (potatoes, corn, protein = 'tofu')
+  puts "What a nutritious meal!"
+  meal = "A plate of #{protein} with #{potatoes} and #{corn}."
+  puts meal
+  meal 
+end
+
